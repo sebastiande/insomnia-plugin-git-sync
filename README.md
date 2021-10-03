87702ef6e9ee4d4c8c0fb8b2a53ac271f9054fc8
Sebastian Dernbach <sebastian.dernbach@ionos.com>
2021-10-03 17:23:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Git sync for Insomnia
+
+This plugin can sync service calls of a project to git and pull from it.
+
+## Insomnia Documentation:
+* https://docs.insomnia.rest/insomnia/introduction-to-plugins
+
+# Install this plugin
+
+Navigate to the Insomnia plugins folder (Preferences > Plugins > Reveal Plugins Folder).
+Run the following command within this folder
+
+```
+npm install insomnia-plugin-git-sync
+```
+
+Hit "Reloard Plugins" within Insomnia.
---
+++ index.js
@@ -0,0 +1,149 @@
+const fs = require('fs');
+
+async function getRepositoryUrl(context, workspaceId) {
+  let repo = await context.store.getItem('repository_' + workspaceId) || false;
+  if (repo === false) {
+    repo = await context.store.getItem('repository_global') || false;
+  }
+  if (repo === false) {
+    return '';
+  }
+  return repo;
+}
+
+function setRepositoryUrl(context, workspaceId, repoUrl) {
+  context.store.setItem('repository_' + workspaceId, repoUrl);
+  context.store.setItem('repository_global', repoUrl);
+}
+
+async function checkForRepoUrl(context, data) {
+  if (!context || !data || !data.workspace || !data.workspace._id) {
+    return false;
+  }
+
+  const repoUrl = await getRepositoryUrl(context, data.workspace._id);
+  if (repoUrl === '') {
+    context.app.alert('Error doing action', 'Please setup at least one GIT repository url for this or any workspace first!');
+    return false;
+  }
+  return true;
+}
+
+function getWorkingDir(workspace) {
+  if (!workspace || !workspace._id) {
+    return false;
+  }
+  let folder = __dirname + '/git';
+  if (!fs.existsSync(folder)){
+      fs.mkdirSync(folder);
+  }
+  folder = __dirname + '/git/' + workspace._id;
+  if (!fs.existsSync(folder)){
+      fs.mkdirSync(folder);
+  }
+  return folder;
+}
+
+function getSimpleGit(workspace) {
+  const folder = getWorkingDir(workspace);
+  return require('simple-git/promise')(folder);
+}
+
+async function initialiseRepo(sGit, context, workspace) {
+  const workDirectory = getWorkingDir(workspace);
+  const repoUrl = await getRepositoryUrl(context, workspace._id);
+  await sGit.clone(repoUrl, workDirectory);
+}
+
+function sanitizeString(str){
+    str = str.replace(/[^a-z0-9\._-]/gim,"_");
+    return str.trim();
+}
+
+module.exports.workspaceActions = [
+  {
+    label: 'Push to GIT Repository',
+    action: async (context, data) => {
+      if (await checkForRepoUrl(context, data) === false) {
+        return;
+      }
+      const exp = await context.data.export.insomnia({
+        includePrivate: true,
+        format: 'json',
+        workspace: data.workspace,
+      });
+
+      if (!exp) {
+        return;
+      }
+
+      const sGit = getSimpleGit(data.workspace);
+      const isGit = await sGit.checkIsRepo();
+      if (!isGit) {
+           initialiseRepo(sGit, context, data.workspace);
+       };
+
+      const expFilename = getWorkingDir(data.workspace) + '/' + sanitizeString(data.workspace.name) + '.json';
+      fs.writeFileSync(expFilename, exp);
+      await sGit.add(expFilename);
+      await sGit.commit('Commit');
+      await sGit.push('origin/master');
+
+      // TODO show alert
+    },
+  },
+  {
+    label: 'Pull from GIT Repository',
+    action: async (context, data) => {
+      if (await checkForRepoUrl(context, data) === false) {
+        return;
+      }
+      const sGit = getSimpleGit(data.workspace);
+      const isGit = await sGit.checkIsRepo();
+      if (!isGit) {
+           initialiseRepo(sGit, context, data.workspace);
+       };
+
+      const impFilename = getWorkingDir(data.workspace) + '/' + sanitizeString(data.workspace.name) + '.json';
+      if (!fs.existsSync(impFilename)) {
+        // TODO alert no config found
+        return;
+      }
+      fs.readFile(impFilename, "utf8", function(err, fileContent) {
+        context.data.import.raw(fileContent, {
+          workspaceId: data.workspace._id,
+        });
+      });
+
+      // TODO show alert
+    },
+  },
+  {
+    label: 'Set GIT Repository URL',
+    action: async (context, data) => {
+      console.log(context);
+      console.log(data);
+      if (!data || !data.workspace || !data.workspace._id) {
+        context.app.alert('Error reading workspace', 'Could not get workspace config');
+        return;
+      }
+      let repoUrl = false;
+      try {
+        repoUrl = await context.app.prompt(
+          'Insert the GIT Repository URL for Workspace ' + data.workspace.name + ':',
+          {
+            defaultValue: await getRepositoryUrl(context, data.workspace._id),
+            submitName: 'Save',
+            cancelable: true
+          }
+        );
+      } catch(error) {
+      }
+
+      if (repoUrl === false) {
+        return;
+      }
+      setRepositoryUrl(context, data.workspace._id, repoUrl);
+    },
+  }
+];
